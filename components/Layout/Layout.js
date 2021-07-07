ce7fa8a9a35e209d72aac916b7b08c8eab4ddf3e
Shalom David Effiom <shalom@gravitasfinancellc.com>
2021-07-07 01:54:31 +0100

    More SEO-related changes

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -12,27 +12,27 @@ const Layout = (props) => {
 				<meta name="author" content="Shalom Effiom" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
 				<meta name="theme-color" content={props.themeColour ? props.themeColour : '#FEC89A'} />
-				<meta name="title" content={props.title ? props.title : 'GraphCMS/NextJs Blog Starter By Shalom Effiom'} />
+				<meta name="title" content={props.title ? props.title : 'Nextjs/GraphCMS Blog Starter By Shalom Effiom'} />
 				<meta name="description" content={props.description ? props.description : 'Hit the ground running with Next.js & GraphCMS'} />
 				<meta name="keywords" content={props.keywords ? Object.values(props.keywords).join(', ') : ''} />
 
-				<meta property="og:title" content={props.title ? props.title : 'GraphCMS/NextJs Blog Starter By Shalom Effiom'} />
+				<meta property="og:title" content={props.title ? props.title : 'Nextjs/GraphCMS Blog Starter By Shalom Effiom'} />
 				<meta property="og:description" content={props.description ? props.description : 'Hit the ground running with Next.js & GraphCMS'} />
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content={props.url ? props.url : 'https://graphcmsnextjs.vercel.app/'} />
-				<meta property="og:title" content={props.title ? props.title : 'GraphCMS/NextJs Blog Starter By Shalom Effiom'} />
+				<meta property="og:url" content={props.url ? props.url : 'https://nextgraphify.vercel.app/'} />
+				<meta property="og:title" content={props.title ? props.title : 'Nextjs/GraphCMS Blog Starter By Shalom Effiom'} />
 				<meta property="og:description" content={props.description ? props.description : 'content="Hit the ground running with Next.js & GraphCMS'} />
 				<meta property="og:image" content={props.image ? props.image : '/vercel.svg'} />
 
 				<meta name="twitter:card" content="summary" />
 				<meta name="twitter:site" content="@sire_shalom" />
-				<meta name="twitter:title" content={props.title ? props.title : 'GraphCMS/NextJs Blog Starter By Shalom Effiom'} />
+				<meta name="twitter:title" content={props.title ? props.title : 'Nextjs/GraphCMS Blog Starter By Shalom Effiom'} />
 				<meta name="twitter:description" content={props.description ? props.description : 'Hit the ground running with Next.js & GraphCMS'} />
 				<meta name="twitter:creator" content="@sire_shalom" />
-				<meta name="twitter:url" property="og:url" content={props.url ? props.url : 'https://graphcmsnextjs.vercel.app/'} />
+				<meta name="twitter:url" property="og:url" content={props.url ? props.url : 'https://nextgraphify.vercel.app/'} />
 				<meta name="twitter:image" property="og:image" content={props.image ? props.image : '/vercel.svg'} />
 
-				<title>{props.title ? props.title : 'GraphCMS/NextJs Blog Starter By Shalom Effiom'}</title>
+				<title>{props.title ? props.title : 'Nextjs/GraphCMS Blog Starter By Shalom Effiom'}</title>
 
 				<link rel="icon" href="/favicon.ico" />
 				<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
